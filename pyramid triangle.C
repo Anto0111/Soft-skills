fea4c434177b444604ad4d83b6b4e822b4a151b5
Anto_Anisten <143519879+Anto0111@users.noreply.github.com>
2024-08-14 11:58:23 +0530

    Create pyramid triangle.C

---
+++ pyramid triangle.C
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    for(int i=0;i<=5;i++)
+    {
+        for(int k=i;k<=5;k++)
+             {
+                  printf(" ");
+             }
+         for(int j=0;j<=i;j++)
+         {
+          printf("*");
+          printf(" ");
+         }
+       printf("\n");
+       
+       
+    }
+}
