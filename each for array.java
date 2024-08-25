a69d1af660bc041337581cc1fbe4c7bbbaadb419
Anto_Anisten <143519879+Anto0111@users.noreply.github.com>
2024-08-26 00:26:54 +0530

    Create each for array.java

---
+++ each for array.java
@@ -0,0 +1,16 @@
+public class Tryarray {
+    
+    public static void main(String[] args) {
+        
+        String[] a={"Anto","vinoth"};
+
+        for(String b : a)
+        
+
+            System.out.println(b);
+
+        
+
+    }
+
+}
